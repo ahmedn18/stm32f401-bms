21359942117d44d8e3770bbe5cf69977cc6038f5
Their MASA <masamostafa2017@gmail.com>
2026-08-23 14:06:54 +0300

    finished the rcc.init functon , added some register addresses to  stm.h

--- src/rcc.h
+++ src/rcc.h
@@ -1,3 +1,5 @@
+#ifndef RCC_H
+#define RCC_H
 
 # define SET_BIT(REG,BIT)     (REG |= (1 << BIT))
 # define CLEAR_BIT(REG,BIT)   (REG &= ~(1 << BIT))
@@ -8,3 +10,4 @@
 // RCC APB2 , RCC APB2ENBR , for ADC clock enable
 // ADC_CCR,Bits 17:16 ADCPRE: ADC prescaler
 void rcc_init(void);
+#endif
